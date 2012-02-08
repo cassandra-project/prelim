9c9b3cb107c18a8e38ffa52304c33da13848b6af
Konstantinos N. Vavliakis <kvavliak@issel.ee.auth.gr>
2012-02-08 21:10:58 +0200

    Graph updates

--- src/eu/cassandra/gui/Chart.java
+++ src/eu/cassandra/gui/Chart.java
@@ -1,46 +1,15 @@
 package eu.cassandra.gui;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Shape;
-import java.awt.geom.Rectangle2D;
-import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Vector;
-
-import javax.swing.JFrame;
-
-import org.apache.log4j.Logger;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.axis.DateTickUnit;
-import org.jfree.chart.axis.DateTickUnitType;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.plot.DefaultDrawingSupplier;
-import org.jfree.chart.plot.DrawingSupplier;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.time.RegularTimePeriod;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.time.Week;
 import org.jfree.data.xy.XYDataset;
 
 /**
  * A time series chart.
  */
 public class Chart {
-
-	private static Logger logger = Logger.getLogger(Chart.class);
-//	private Font font = new Font("Arial",Font.BOLD,25);
 
 	/**
 	 * Creates a chart.
@@ -55,164 +24,17 @@ public class Chart {
 				"Time", 
 				"Consumption (W)",
 				dataset, 
-				true, 
+				false, 
 				true, 
 				true
 		);
-////		chart.getLegend().setItemFont(font);
-//		final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-//		renderer.setSeriesLinesVisible(0, true);
-//		renderer.setSeriesShapesVisible(0, true);
-//		renderer.setSeriesPaint(0, Color.RED);
-//		renderer.setSeriesStroke(0, new BasicStroke(4f));
 
 		DateAxis axis = (DateAxis) chart.getXYPlot().getDomainAxis();
 		axis.setDateFormatOverride(new SimpleDateFormat("mm:HH dd/MM"));//MMM/yy
 
-//		XYPlot axisY = chart.getXYPlot();
-//		ValueAxis valueAxis = axisY.getRangeAxis();
-
 		chart.setAntiAlias(true);
 		chart.setTextAntiAlias(true);
 
-		//renderer.setSeriesPaint(chart.getXYPlot().getDataset().getSeriesCount()-1,Color.BLUE);
-
 		return chart;
 	}
-
-
-	/**
-	 * Creates a sample dataset.
-	 * 
-	 * @param potentialEventMap
-	 * @return
-	 */
-	//	public TimeSeriesCollection createDataset(Vector<DateItemID> itemDataset,boolean allColor, boolean allSizes) {
-	//		TimeSeriesCollection dataset = new TimeSeriesCollection();
-	//		TimeSeries timeSeries = new TimeSeries("ALL");
-	//		for(DateItemID itemWithDate : itemDataset) {
-	//			Calendar date = itemWithDate.getDate();
-	//			ItemInstancePurchased item = itemWithDate.getItem();
-	//
-	//			Week week = new Week(date.getTime());
-	//			if(timeSeries.getDataItem(week) != null)
-	//				timeSeries.addOrUpdate(week,timeSeries.getDataItem(week).getValue().intValue() + item.getItemQuantity());
-	//			else
-	//				timeSeries.addOrUpdate(week,item.getItemQuantity());
-	//		}
-	//
-	//		HashMap<String,TimeSeries> allColorTimeSeries = new HashMap<String,TimeSeries>();
-	//		HashMap<String,TimeSeries> allSizeTimeSeries = new HashMap<String,TimeSeries>();
-	//
-	//		if(allColor) {
-	//			addColorSizeTimeSeries(itemDataset,allColorTimeSeries,allColor);
-	//		}
-	//		else if(allSizes) {
-	//			addColorSizeTimeSeries(itemDataset,allSizeTimeSeries,!allSizes);
-	//		}
-	//
-	//		//dataset.addSeries(timeSeries);
-	//		for(TimeSeries tSeries : allColorTimeSeries.values())
-	//			dataset.addSeries(tSeries);
-	//		for(TimeSeries tSeries : allSizeTimeSeries.values())
-	//			dataset.addSeries(tSeries);
-	//
-	//		return dataset;
-	//	}
-
-	/**
-	 * 
-	 * @param dateValueInfoPerCustomerTypeMap
-	 * @return
-	 */
-	//	public TimeSeriesCollection createWhenCustomerTypesBuyDataset(HashMap<String,Vector<DateValueInfo>> dateValueInfoPerCustomerTypeMap) {
-	//		TimeSeriesCollection dataset = new TimeSeriesCollection();
-	//		Vector<String> customerTypes = new Vector<String>();
-	//		customerTypes.add("Valueless");
-	//		customerTypes.add("Uncertain");
-	//		customerTypes.add("Loyal");
-	//		customerTypes.add("High Roller");
-	//		customerTypes.add("NA");
-	//
-	//		for(String customerType : customerTypes){
-	//			TimeSeries timeSeries = new TimeSeries(customerType);
-	//			Vector<DateValueInfo> dateValueInfoPerCustomerType = dateValueInfoPerCustomerTypeMap.get(customerType);
-	//			if(dateValueInfoPerCustomerType != null) {
-	//				for(DateValueInfo dateValueInfoItem : dateValueInfoPerCustomerType) {
-	//					Week week = new Week(dateValueInfoItem.getDate());
-	//					if(timeSeries.getDataItem(week) != null)
-	//						timeSeries.addOrUpdate(week,timeSeries.getDataItem(week).getValue().intValue() + 1);
-	//					else
-	//						timeSeries.addOrUpdate(week,1);
-	//				}
-	//			}
-	//			Iterator iter = timeSeries.getTimePeriods().iterator();
-	//
-	//			if(!timeSeries.isEmpty()) {
-	//				Week startWeek =  (Week)timeSeries.getTimePeriod(0);
-	//				Week endWeek =  (Week)timeSeries.getTimePeriod(timeSeries.getItemCount()-1);
-	//				while(startWeek.getStart().before(endWeek.getStart())) {
-	//					if(timeSeries.getDataItem(startWeek) != null) 
-	//						System.out.println("\t" + startWeek + "\t" + timeSeries.getDataItem(startWeek).getValue());
-	//					else {
-	//						timeSeries.addOrUpdate(startWeek,0);
-	//						System.out.println("\t" + startWeek + " updated");
-	//					}
-	//					startWeek = (Week)startWeek.next();
-	//				}
-	//			}
-	//
-	//			dataset.addSeries(timeSeries);
-	//		}
-	//		return dataset;
-	//	}
-
-
-	//	private void addColorSizeTimeSeries(Vector<DateItemID> itemDataset,	HashMap<String,TimeSeries> allColorSizeTimeSeries,boolean useColor) {
-	//		for(DateItemID itemWithDate : itemDataset) {
-	//			String attribute = null;
-	//			if(useColor)
-	//				attribute = itemWithDate.getItem().getItemColor().split(" ")[0];
-	//			else
-	//				attribute = itemWithDate.getItem().getItemSize();
-	//			TimeSeries colorSizeTimeSeries = null;
-	//			if(allColorSizeTimeSeries.containsKey(attribute))
-	//				colorSizeTimeSeries = allColorSizeTimeSeries.get(attribute);
-	//			else {
-	//				colorSizeTimeSeries = new TimeSeries(attribute);
-	//				allColorSizeTimeSeries.put(attribute,colorSizeTimeSeries);
-	//			}
-	//
-	//			Calendar date = itemWithDate.getDate();
-	//			ItemInstancePurchased item = itemWithDate.getItem();
-	//			Week month = new Week(date.getTime());
-	//			if(colorSizeTimeSeries.getDataItem(month) != null)
-	//				colorSizeTimeSeries.addOrUpdate(month,colorSizeTimeSeries.getDataItem(month).getValue().intValue() + item.getItemQuantity());
-	//			else
-	//				colorSizeTimeSeries.addOrUpdate(month,item.getItemQuantity());
-	//		}
-	//	}
-
-
-	/**
-	 * 
-	 * @param dataset
-	 * @param title
-	 */
-//	public void saveJChart(String title,JFreeChart chart) {
-//		try {
-//			String fileName = title +  ".jpg";
-//			File file = new File(fileName);
-//			file.createNewFile();
-//			ChartUtilities.saveChartAsJPEG(file, chart, 3000, 1500);
-//		} catch (IOException e) {
-//			logger.error(e.getMessage());
-//			e.printStackTrace();
-//		}
-//
-//	}
-
-
-
-
 }
--- src/eu/cassandra/gui/MainInterface.java
+++ src/eu/cassandra/gui/MainInterface.java
@@ -2,21 +2,11 @@ package eu.cassandra.gui;
 
 import javax.swing.*;
 
-import org.apache.log4j.Logger;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.general.DatasetChangeEvent;
-import org.jfree.data.general.DatasetChangeListener;
-import org.jfree.data.statistics.SimpleHistogramDataset;
-import org.jfree.data.time.Hour;
 import org.jfree.data.time.Minute;
-import org.jfree.data.time.RegularTimePeriod;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.xy.DefaultXYDataset;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 import eu.cassandra.entities.installations.Installation;
 import eu.cassandra.platform.Observer;
@@ -32,26 +22,17 @@ import java.util.Calendar;
  *
  */
 public class MainInterface implements Runnable {
-	TimeSeriesCollection dataset;
-	private static Logger logger = Logger.getLogger(MainInterface.class);
 
-	ChartPanel chartPanel ;
-	
+	private TimeSeriesCollection dataset;
 	// Initialize all swing objects.
 	private JFrame f = new JFrame("Cassanda GUI"); 
-
 	private JPanel buttonPanel = new JPanel();  
 	private JScrollPane buttonScrollPane = new JScrollPane(buttonPanel);
 	private JScrollPane graphScrollPane;
-
 	// Buttons some there is something to put in the panels
 	private JButton startButton = new JButton("Start");
-	private JButton pauseButton = new JButton("Pause");
-	private JButton stopButton = new JButton("Stop");
 	private JButton exitButton = new JButton("Exit");
-
 	private JComboBox installationCombo = new JComboBox();
-
 	// Menu
 	private JMenuBar menuBar = new JMenuBar(); // Menubar
 	private JMenu menuFile = new JMenu("File"); // File Entry on Menu bar
@@ -59,57 +40,47 @@ public class MainInterface implements Runnable {
 	private JMenu menuHelp = new JMenu("Help"); // Help Menu entry
 	private JMenuItem menuItemAbout = new JMenuItem("About"); // About Entry
 
-
+	/**
+	 * 
+	 */
 	public MainInterface(){
 
 		installationCombo.addActionListener(new ListenInstallationComboBox());
 		installationCombo.setPreferredSize(new Dimension(300, 20));
 
 		startButton.addActionListener(new ListenStartButton());
-		pauseButton.addActionListener(new ListenPauseButton());
-		stopButton.addActionListener(new ListenStopButton());
 		exitButton.addActionListener(new ListenExitButton());
 
-		// Set menubar
 		f.setJMenuBar(menuBar);
 
-		//Build Menus
-		menuFile.add(menuItemQuit);  // Create Quit line
-		menuHelp.add(menuItemAbout); // Create About line
-		menuBar.add(menuFile);        // Add Menu items to form
+		menuFile.add(menuItemQuit);  
+		menuHelp.add(menuItemAbout); 
+		menuBar.add(menuFile);       
 		menuBar.add(menuHelp);
 
 
-		TimeSeries series = new TimeSeries("", Minute.class);
+		TimeSeries series = new TimeSeries("");
 		dataset = new TimeSeriesCollection(series);
 		Chart c = new Chart();
 		JFreeChart chart = c.createChart("Consumption of: 1", dataset);
-		chartPanel = new ChartPanel(chart); 
+		ChartPanel chartPanel = new ChartPanel(chart); 
 		graphScrollPane = new JScrollPane(chartPanel);
 
-		// Add Buttons
 		buttonPanel.add(startButton);
-		buttonPanel.add(pauseButton);
-		buttonPanel.add(stopButton);
 		buttonPanel.add(installationCombo);
 		buttonPanel.add(exitButton);
 
-		// Setup Main Frame
 		f.getContentPane().setLayout(new BorderLayout());
 		f.getContentPane().add(graphScrollPane, BorderLayout.CENTER);
 		f.getContentPane().add(buttonScrollPane, BorderLayout.SOUTH);
 
-		// Allows the Swing App to be closed
 		f.addWindowListener(new ListenCloseWdw());
-
-		//Add Menu listener
 		menuItemQuit.addActionListener(new ListenMenuQuit());
 	}
 
 	Observer obs = null;
 
 	public class ListenStartButton implements ActionListener {
-
 		public void actionPerformed(ActionEvent e) {
 			startButton.setEnabled(false);	
 			obs = new Observer();
@@ -120,41 +91,41 @@ public class MainInterface implements Runnable {
 
 	public void run() {
 		obs.setup();
+		obs.simulate();
+		installationCombo.addItem("All installations");
 		for(Installation inst : Observer.installations) {
 			installationCombo.addItem(inst.getId());
 		}
-		obs.simulate();
 		startButton.setEnabled(true);	
-	}
-//
-//	public class ListenData implements DatasetChangeListener  {
-//		public void datasetChanged(DatasetChangeEvent arg0) {
-//			//   generateChartMethod();         
-//		}
-//	}
-
-	public class ListenPauseButton implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-		}
-	}
-
-	public class ListenStopButton implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-		}
 	}
 
 	public class ListenInstallationComboBox implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			TimeSeries	series = new TimeSeries("", Minute.class);
+			TimeSeries	series = new TimeSeries("");
 			//series.clear();
 			Calendar cal = Calendar.getInstance();
-			Installation inst = Observer.installations.get(installationCombo.getSelectedIndex());
-			float[] values = inst.getRegistry().getValues();
 			Minute minute = new Minute();
 			minute.peg(cal);
-			for(int i =0; i<values.length;i++) {
-				minute = new Minute(minute.next().getStart());
-				series.add(minute, values[i]);
+			if(installationCombo.getSelectedIndex() == 0){
+				if(Observer.installations.size() > 0) {
+					for(int i=0; i <Observer.installations.get(0).getRegistry().getValues().length;i++ ) {
+						float mean = 0;
+						for(Installation inst: Observer.installations) {
+							mean += inst.getRegistry().getValues()[i];
+						}		
+						//mean /= Observer.installations.size();
+						minute = new Minute(minute.next().getStart());
+						series.add(minute, mean);
+					}
+				}
+			}
+			else{
+				Installation inst = Observer.installations.get(installationCombo.getSelectedIndex()+1);
+				float[] values = inst.getRegistry().getValues();
+				for(int i =0; i<values.length;i++) {
+					minute = new Minute(minute.next().getStart());
+					series.add(minute, values[i]);
+				}
 			}
 			dataset.removeSeries(0);
 			dataset.addSeries(series);
